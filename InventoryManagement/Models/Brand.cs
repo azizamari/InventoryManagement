b2cccace21c4ee0991aa05a5a3034cdc825c8b7e
azizamari <azizamari2003@gmail.com>
2021-05-02 13:32:45 +0100

    Add Brand Model

---
+++ InventoryManagement/Models/Brand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Models
+{
+    public class Brand
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Category { get; set; }
+    }
+}
