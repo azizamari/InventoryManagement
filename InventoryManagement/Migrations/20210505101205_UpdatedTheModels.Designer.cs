e677acd68f255012f2c85e8156ad022d066c70e4
azizamari <azizamari2003@gmail.com>
2021-05-05 11:44:26 +0100

    New Migrations

--- InventoryManagement/Migrations/20210505101205_UpdatedTheModels.Designer.cs
+++ InventoryManagement/Migrations/20210505101205_UpdatedTheModels.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using InventoryManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace InventoryManagement.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210505101205_UpdatedTheModels")]
+    partial class UpdatedTheModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ InventoryManagement/Migrations/20210505101205_UpdatedTheModels.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventoryManagement.Migrations
+{
+    public partial class UpdatedTheModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Brands_Categories_CategoryName",
+                table: "Brands");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Brands_CategoryName",
+                table: "Brands");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryName",
+                table: "Brands");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Brands",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Brands");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryName",
+                table: "Brands",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Brands_CategoryName",
+                table: "Brands",
+                column: "CategoryName");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Brands_Categories_CategoryName",
+                table: "Brands",
+                column: "CategoryName",
+                principalTable: "Categories",
+                principalColumn: "Name",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
