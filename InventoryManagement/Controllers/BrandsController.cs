07ebfe11f4d7275be5bc0798b2040d17d892e494
azizamari <azizamari2003@gmail.com>
2021-05-05 11:43:11 +0100

    Add ability to Add brand, get a single brand, get all brands

--- InventoryManagement/Controllers/BrandsController.cs
+++ InventoryManagement/Controllers/BrandsController.cs
@@ -22,13 +22,67 @@ namespace InventoryManagement.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            var brands = _db.Brands;
+            return View(brands);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
             return View();
         }
+
         [HttpPost]
-        public IActionResult Index(Brand brand)
+        public async Task<IActionResult> Add(Brand brand)
         {
-            var b = brand;
-            return RedirectToAction(nameof(Index));
+            // save to db
+            if (ModelState.IsValid)
+            {
+                await _db.Brands.AddAsync(brand);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
         }
+
+        [HttpGet]
+        [Route("{controller}/{id:int}")]
+        public async Task<IActionResult> GetBrand(int id)
+        {
+            var brand = await _db.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return View("Brand", brand);
+        }
+
+        //[HttpDelete]
+        //[Route("{M}")]
+        //public async Task<IActionResult> DeleteCategory(int id)
+        //{
+        //    // delete category
+        //    var category = await _db.Categories.FindAsync(new { id = id });
+        //    if (category == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    _db.Categories.Remove(category);
+        //    await _db.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        //[HttpPatch]
+        //[Route("{id:int}")]
+        //public async Task<IActionResult> UpdateCategory(int id, Category category)
+        //{
+        //    // update category
+        //    if (ModelState.IsValid)
+        //    {
+        //        _db.Categories.Update(category);
+        //        await _db.SaveChangesAsync();
+        //    }
+        //    return RedirectToAction(nameof(Index));
+        //}
     }
 }
