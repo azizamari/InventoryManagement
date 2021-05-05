1b5bd88dbd6e21283627409054aa128df0edea0a
azizamari <azizamari2003@gmail.com>
2021-05-06 00:45:51 +0100

    Add Products Controller

---
+++ InventoryManagement/Controllers/ProductsController.cs
@@ -0,0 +1,87 @@
+﻿using InventoryManagement.Data;
+using InventoryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Controllers
+{
+    public class ProductsController: Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ProductsController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var products = _db.Products;
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product product)
+        {
+            // save to db
+            if (ModelState.IsValid)
+            {
+                await _db.Products.AddAsync(product);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        [Route("{controller}/{id:int}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View("Brand", product);
+        }
+
+        [HttpDelete]
+        [Route("{controller}/{id:int}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            // delete products
+            var product = await _db.Products.FindAsync(new { id = id });
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _db.Products.Remove(product);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPatch]
+        [Route("{id:int}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            // update Product
+            if (ModelState.IsValid)
+            {
+                _db.Products.Update(product);
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
