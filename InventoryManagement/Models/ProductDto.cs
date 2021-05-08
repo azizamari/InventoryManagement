c02e0f70284a6683ad07ca5d8fc6091da8f4f35b
azizamari <azizamari2003@gmail.com>
2021-05-08 21:45:35 +0100

    Add product data transfer object (will be used in form to add product)

---
+++ InventoryManagement/Models/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Models
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string CategoryName { get; set; }
+        public string BrandName { get; set; }
+    }
+}
