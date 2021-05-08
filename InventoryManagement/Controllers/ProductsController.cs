2cdd26767d85bcd653442ea840a3e143dde16170
azizamari <azizamari2003@gmail.com>
2021-05-08 21:39:06 +0100

    Changes to produtcs controller

--- InventoryManagement/Controllers/ProductsController.cs
+++ InventoryManagement/Controllers/ProductsController.cs
@@ -1,10 +1,6 @@
 ﻿using InventoryManagement.Data;
 using InventoryManagement.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace InventoryManagement.Controllers
