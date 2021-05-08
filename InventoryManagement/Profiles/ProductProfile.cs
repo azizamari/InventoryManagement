ef47929fb81301e04780d2002852552a91f7e412
azizamari <azizamari2003@gmail.com>
2021-05-08 22:07:29 +0100

    Create profile to map from ProductDto to Product

---
+++ InventoryManagement/Profiles/ProductProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using InventoryManagement.Models;
+
+namespace InventoryManagement.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<ProductDto, Product>()
+                .ForMember(dest=>dest.Category.Name,opt=>opt.MapFrom(src=>src.CategoryName))
+                .ForMember(dest=>dest.Brand.Id,opt=>opt.MapFrom(src=>src.BrandId));
+                .ForMember(dest=>dest.Brand.Id,opt=>opt.MapFrom(src=>src.BrandId));
+        }
+    }
+}
