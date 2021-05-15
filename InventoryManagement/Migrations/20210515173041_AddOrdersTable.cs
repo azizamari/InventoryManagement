6379e38fee0d3ee484678024d7fa47684b66eb4a
azizamari <azizamari2003@gmail.com>
2021-05-15 18:31:14 +0100

    Add orders table to database and migrations

---
+++ InventoryManagement/Migrations/20210515173041_AddOrdersTable.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace InventoryManagement.Migrations
+{
+    public partial class AddOrdersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BuyerName = table.Column<string>(nullable: false),
+                    BuyerPhoneNumber = table.Column<string>(nullable: false),
+                    Status = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_OrderId",
+                table: "Products",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Orders_OrderId",
+                table: "Products",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Orders_OrderId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_OrderId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Products");
+        }
+    }
+}
