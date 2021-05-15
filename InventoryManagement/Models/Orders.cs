449d0afcd9a989ee2ee3e4bf8873a0a260f45e07
azizamari <azizamari2003@gmail.com>
2021-05-15 18:01:05 +0100

    Add orders model

---
+++ InventoryManagement/Models/Orders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Models
+{
+    public class Orders
+    {
+        [Key]
+        public Guid Id;
+        [Required]
+        public ICollection<(Product Product, int Quantity)> products;
+        [Required]
+        public string BuyerName;
+        [Required]
+        public string BuyerPhoneNumber;
+        [Required]
+        public string Status;
+        // delivered - canceled - on going
+    }
+}
